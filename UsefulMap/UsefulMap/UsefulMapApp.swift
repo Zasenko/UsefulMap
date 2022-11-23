85aa8bfd931ac6e202ca5d11ea1930bb5d962dd0
Zasenko <dmitry.zasenko@gmail.com>
2022-11-23 18:31:16 +0100

    Empty Project

---
+++ UsefulMap/UsefulMap/UsefulMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsefulMapApp.swift
+//  UsefulMap
+//
+//  Created by Dmitry Zasenko on 23.11.22.
+//
+
+import SwiftUI
+
+@main
+struct UsefulMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
